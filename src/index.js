673d371a0fb74e2e91ad93ffb0bd9951356b15ab
Collins Aniesodo <colinsanisedo@gmail.com>
2025-01-21 15:53:26 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -160,6 +160,379 @@ const workData = [
     tags: ["video animation"],
     type: "video",
   },
+
+  //phase four
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza3.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "food-pasta-ads-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "vehicle branding",
+    imgUrl: "truck-lorry-branding.jpg",
+    tags: ["vehicle branding"],
+    type: "image",
+  },
+  {
+    title: "promo booths",
+    imgUrl: "promo-trade-booth-stand-fair-kiosk.jpg",
+    tags: ["promo booths"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "cocktail-logo-cocktail-bar.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_m4.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //fifth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza4.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "kindergarten-kids-poster-flyer.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "promo booths",
+    imgUrl: "promo-trade-booth-stand-fair-kiosk-elements.jpg",
+    tags: ["promo booths"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "cocktail-logo-cocktail-bar_1.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_m5.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //sixth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza5.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "kindergarten-kids-poster-instagram-ad.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "healthy-food-logo.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_z1.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+   //seventh phase
+   {
+    title: "labels & packaging",
+    imgUrl: "ambalaza6.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "magazine-ad-healthy-food.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "logo-jci-united- nations-corporation.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_z2.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //eigth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza7.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "magazine-ads-poster-product.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "pet-dog-clothing-clothes-logo.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_z3.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //nineth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza8.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "music-festival-jazz-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "pet-dog-clothing-clothes-logos.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_z4.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //tenth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza9.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "music-festival-party-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "logo",
+    imgUrl: "restaurant-pub-logo.jpg",
+    tags: ["logo"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Legend_z5.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+   //eleventh phase
+   {
+    title: "labels & packaging",
+    imgUrl: "ambalaza10.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "music-night-club-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+  {
+    title: "T-shirt",
+    imgUrl: "Behance-Naslovnice-majice.jpg",
+    tags: ["T-shirt"],
+    type: "image",
+  },
+
+  //twelveth phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza11.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "music-party-event-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+
+   //thirteen phase
+   {
+    title: "labels & packaging",
+    imgUrl: "ambalaza12.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "party-night-club-instagram-post-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+
+  // fouteen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza13.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "product-flyer-instagram-post.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+
+  // fifteen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza14.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+  {
+    title: "Posters & flyers",
+    imgUrl: "sport-tournament-poster.jpg",
+    tags: ["Posters & flyers"],
+    type: "image",
+  },
+
+  // sixteen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza15.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // seventeen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza16.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // eighteen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza17.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // ninteen phase
+  {
+    title: "labels & packaging",
+    imgUrl: "ambalaza18.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty phase
+  {
+    title: "labels & packaging",
+    imgUrl: "bag-packaging-branding.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty-one phase
+  {
+    title: "labels & packaging",
+    imgUrl: "box-dieline-packaging-dou-pack.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty-two phase
+  {
+    title: "labels & packaging",
+    imgUrl: "box-packaging-brandind-medicine-cure.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty-three phase
+  {
+    title: "labels & packaging",
+    imgUrl: "chocolate-bar-design.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+   // twenty-four phase
+   {
+    title: "labels & packaging",
+    imgUrl: "chocolate-bar-design.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  
+   // twenty-five phase
+   {
+    title: "labels & packaging",
+    imgUrl: "chocolate-raw-bar-design_print.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty-six phase
+  {
+    title: "labels & packaging",
+    imgUrl: "label-box-packaging-doypack-laundry-detergent.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
+
+  // twenty-seven phase
+  {
+    title: "labels & packaging",
+    imgUrl: "laundry- detergent-design-label-packaging.jpg",
+    tags: ["labels & packaging"],
+    type: "image",
+  },
 ];
 
 
