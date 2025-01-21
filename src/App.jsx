0a37b22ad5dd9f605cb0202709848e5b7745c788
Collins Aniesodo <colinsanisedo@gmail.com>
2025-01-21 16:09:30 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { About,Footer} from './container'
+import { About,Footer,Portfolio} from './container'
 import { Navbar } from './components'
 import './App.scss';
 import Faq from './container/faq/Faq';
-import Portfolio from './container/Portfolio/Portfolio';
+
 
 
 
